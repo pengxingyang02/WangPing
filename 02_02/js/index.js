2715ed561129833bd790465a9eea493779ca3da0
pengxingyang <122389908+pengxingyang02@users.noreply.github.com>
2025-11-14 23:52:02 +0800

    Update index.js

--- 02_02/js/index.js
+++ 02_02/js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||02宝宝|对不起|不要生气了！|❤|#rectangle|');
+      S.UI.simulate('|#countdown 3||刘子萌宝宝|对不起|不要生气了！|❤|#rectangle|');
     }
 
     S.Drawing.loop(function () {
